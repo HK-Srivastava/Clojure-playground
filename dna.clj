54897cfd38544aba984ec2105533f3a55137538c
Harsh Srivastava <107634137+HK-Srivastava@users.noreply.github.com>
2023-01-10 23:18:26 +0530

    Update dna.clj
    Better implementation

--- dna.clj
+++ dna.clj
@@ -2,25 +2,16 @@
 
 ;; To count the number of occurrences of a dna-base/nucleotide in a valid strand of DNA
 ;; Throws exception if an invalid nucleotide is provides
-(defn count-of-nucleotide-in-strand [nucleotide strand]
-  (if (or (= \A nucleotide)
-          (= \C nucleotide)
-          (= \G nucleotide)
-          (= \T nucleotide))
-    (count (filter #(= nucleotide %) strand))
-    (throw (IllegalArgumentException. "error"))))
+(defn count-of-nucleotide-in-strand [nucleotide strand] 
+  {:pre [(contains? #{\A \T \C \G} nucleotide)]}
+  (count (filter #(= nucleotide %) strand)))
 
 ;; To count the number of occurences of each nucleotide in a given strand of DNA
 ;; Returns "error" if the DNA strand provided is invalid.
-(defn nucleotide-counts [strand] ;; <- Arglist goes here
-  ;; your code goes here
-  (if (nil? (re-matches #"[^ACGT]+" strand))
-    (let [a (count-of-nucleotide-in-strand \A strand)
-          c (count-of-nucleotide-in-strand \C strand)
-          g (count-of-nucleotide-in-strand \G strand)
-          t (count-of-nucleotide-in-strand \T strand)]
-      {\A a \C c \G g \T t})
-    "error"))
+(defn nucleotide-counts [strand] 
+  (if (re-find #"[^ACGT]+" strand)
+    "error"
+    (merge {\A 0 \T 0 \C 0 \G 0} (frequencies strand))))
 
 
 ;; ------------------------------------------------------------------------------------------
