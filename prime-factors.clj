1aafb35d879c0d4cff403206a31e7f9faa6dabc7
Harsh Srivastava <107634137+HK-Srivastava@users.noreply.github.com>
2023-01-20 01:10:08 +0530

    Update prime-factors.clj

--- prime-factors.clj
+++ prime-factors.clj
@@ -1,5 +1,16 @@
 (ns prime-factors)
 
+(defn of [n]
+  (loop [n n, d 2, l (Math/sqrt n), arr []]
+    (cond (= 1 n) arr
+          (= 0 (rem n d)) (let [q (quot n d)
+                                l (Math/sqrt q)]
+                            (recur q d l (conj arr d)))
+          (> d l) (recur n n n arr)
+          :else (recur n (inc d) l arr))))
+
+;; ------------------------
+;; Using recursion --older implementation
 (defn- nxt-factor [n d]
   (some #(when (= 0 (rem n %)) %) (iterate inc d)))
 
